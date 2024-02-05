f7d877dcf2193160bf076e596078a967e90df9ce
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-05 15:54:10 +0530

    Update and rename colorCoder.js to color.js

---
+++ app/color.js
@@ -0,0 +1,13 @@
+
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+function ColorPair(){
+           this.majorColor;
+           this.minorColor;
+}
+export { MajorColorNames, MinorColorNames, ColorPair };
