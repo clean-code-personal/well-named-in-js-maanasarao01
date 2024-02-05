63790ff9e87cccbf2c747a0f5ae6d07478b3337e
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-05 15:58:13 +0530

    Update color.js

--- app/color.js
+++ app/color.js
@@ -10,4 +10,7 @@ function ColorPair(){
            this.majorColor;
            this.minorColor;
 }
+ColorPair.prototype.toString=function(){
+	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
+}
 export { MajorColorNames, MinorColorNames, ColorPair };
