f49735fcdf6527448db71f056966668ec336c7f3
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-06 14:06:36 +0530

    Update color_manual.js

--- app/color_manual.js
+++ app/color_manual.js
@@ -1,4 +1,19 @@
-const{colorManual}=require('./color_number.js');
+const{ MajorColorNames,MinorColorNames,getPairNumberFromColor,ColorPair,colorManual}=require('./color_number.js');
+
+function colorManual() {
+    let colorCodes = [];
+    for (let majorColor of MajorColorNames) {
+        for (let minorColor of MinorColorNames) {
+            let pair = new ColorPair();
+            pair.majorColor = majorColor;
+            pair.minorColor = minorColor;
+            let pairNumber = getPairNumberFromColor(pair);
+            colorCodes.push(`  ${pairNumber}       -  ${pair.toString()}`);
+        }
+    }
+    return colorCodes.join('\n');
+}
+
 function printManual() {
     let manual = colorManual();
     console.log('\nColor Coding Reference Manual:\n');
