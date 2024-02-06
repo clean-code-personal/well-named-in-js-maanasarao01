7538518a518ad8c94de7792a8e9208a8fe0e602e
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-06 14:35:25 +0530

    Update test.js

--- app/test.js
+++ app/test.js
@@ -10,8 +10,6 @@ function test(){
 			pairNumber = 5;
             testPair1 = getColorFromPairNumber(pairNumber);
             console.log(`[In]Pair Number: ${pairNumber},[Out] Colors:${testPair1}`);
-	                console.log(`Major Color: ${testPair1.majorColor}`);
-                        console.log(`Minor Color: ${testPair1.minorColor}`);
 			console.assert(testPair1.majorColor === "WHITE");
 			console.assert(testPair1.minorColor === "SLATEGRAY");
 			 
@@ -25,14 +23,14 @@ function test(){
 			testPair2.majorColor="YELLOW";
 			testPair2. minorColor ="GREEN";
             pairNumber =getPairNumberFromColor(testPair2);
-            console.log(`[In]Colors: ${testPair2}, [Out] PairNumber: ${pairNumber}`);
+            console.log(`[In]Colors: ${testPair2},   [Out]  PairNumber: ${pairNumber}`);
 			console.assert(pairNumber==18);
 
 			testPair2 = new ColorPair();
 			testPair2.majorColor="RED";
 			testPair2. minorColor ="BLUE";
             pairNumber =getPairNumberFromColor(testPair2);
-            console.log(`[In]Colors: ${testPair2}, [Out] PairNumber: ${pairNumber}`);
+            console.log(`[In]Colors: ${testPair2},   [Out]  PairNumber: ${pairNumber}`);
 			console.assert(pairNumber==6);
 }
 test();
